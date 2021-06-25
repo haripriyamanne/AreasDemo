7d160c5209c36c5a13d0f6ce27e374018724734f
haripriyamanne <haripriyamanne4@gmail.com>
2021-06-25 20:01:35 +0530

    Add project files.

---
+++ AreasDemo/Areas/Department/Controllers/SalesController.cs
@@ -0,0 +1,38 @@
+﻿using AreasDemo.Areas.Department.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AreasDemo.Areas.Department.Controllers
+{
+        [Area("Department")]
+        public class SalesController : Controller
+        {
+            public IActionResult Index()
+            {
+                Location[] salesLocation = new Location[] {
+                new Location
+                {
+                    ID = 1,
+                    Address = "A Road",
+                    City = "India"
+                },
+                new Location
+                {
+                    ID = 2,
+                    Address = "B Road",
+                    City = "London"
+                },
+                new Location
+                {
+                    ID = 3,
+                    Address = "C Road",
+                    City = "New York"
+                },
+            };
+                return View(salesLocation);
+            }
+        }
+    }
---
+++ AreasDemo/Areas/Employee/Controllers/SalesEmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AreasDemo.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class SalesEmployeeController : Controller
+    {
+        [HttpGet]
+
+        public IActionResult GetEmployeeListByLocation(int locationID)
+        {
+            Models.Employee[] employees = new Models.Employee[] {
+                new Models.Employee
+                {
+                    ID = 1,
+                    Name = "Hari",
+                    Designation="Manager",
+                    LocationID=1
+                },
+                new Models.Employee
+                {
+                    ID = 2,
+                    Name = "Priya",
+                    Designation="Developer",
+                    LocationID=1
+                },
+                new Models.Employee
+                {
+                    ID = 3,
+                    Name = "Latha",
+                    Designation="CEO",
+                    LocationID=2
+                },
+                 new Models.Employee
+                {
+                    ID = 4,
+                    Name = "Janu",
+                    Designation="CEO",
+                    LocationID=3
+                },
+            };
+
+            Models.Employee[] empList =
+                employees.Where(item => item.LocationID == locationID).ToArray();
+
+            return View(empList);
+        }
+    }
+}
